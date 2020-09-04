f6d6931d50fba896e9d9ea784ef9085e1556beb0
Rashmi Yadav <yads.rash@gmail.com>
2020-09-04 20:13:28 +0530

    added signup routing

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignupComponent },
+  { path: 'profile', component: ProfilePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
