47260a5072caa7d0c1d4f04059005890c627b7c6
tiwarismriti <smrititiwari95.stm@gmail.com>
2020-09-14 12:47:49 +0530

    Changes done

--- ui/src/app/material/material.module.ts
+++ ui/src/app/material/material.module.ts
@@ -5,7 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatOptionModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 const material = [
   MatGridListModule,
@@ -13,7 +14,8 @@ const material = [
   MatButtonModule,
   MatCardModule,
   MatToolbarModule,
-  MatOptionModule,
+  MatFormFieldModule,
+  MatSelectModule,
 ];
 
 @NgModule({
